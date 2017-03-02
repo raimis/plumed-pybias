56b1056b0843782a1e3134b6963bdf1c47edddb6
Raimondas Galvelis <raimondas.galvelis@gmail.com>
2017-03-02 17:08:17 +0200

    Add initial source code

---
+++ src/pybias.cpp
@@ -0,0 +1,285 @@
+/*
+Copyright (c) 2017 Raimondas Galvelis
+*/
+
+#include <Python.h>
+#include <link.h>
+#include <dlfcn.h>
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <numpy/ndarrayobject.h>
+#include <numpy/npy_math.h>
+#include <mpi4py/mpi4py.h>
+
+#include "plumed/bias/Bias.h"
+#include "plumed/bias/ActionRegister.h"
+#include "plumed/core/Atoms.h"
+#include "plumed/core/PlumedMain.h"
+#include "plumed/tools/Communicator.h"
+
+using namespace std;
+
+namespace PLMD{
+namespace bias{
+
+class PyBias: public Bias
+{
+  vector<Value*> args; // Extra arguments
+
+  PyObject* function; // Python function object
+
+  PyObject* input; // Numpy array for input values
+  PyObject* force; // Numpy array for bias forces
+  PyObject* extra; // Numpy array for extra values
+
+public:
+  static void registerKeywords(Keywords&);
+  explicit PyBias(const ActionOptions&);
+  ~PyBias();
+  void calculate();
+  unsigned getNumberOfExtraArguments() const;
+};
+
+PLUMED_REGISTER_ACTION(PyBias, "PYBIAS")
+
+void PyBias::registerKeywords(Keywords& keys)
+{
+  Bias::registerKeywords(keys);
+  keys.use("ARG");
+  keys.add("compulsory", "FILE", "bias.py",
+           "the name of a file containing a python scrip.");
+  keys.add("optional", "EXTRA",
+           "extra argument");
+  componentsAreNotOptional(keys);
+  keys.addOutputComponent("bias", "default", "bias");
+}
+
+// Pointer to currecnt action
+static Action* action = NULL;
+
+// Implementation of plumed.getStep
+static PyObject* getStep(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  return PyInt_FromLong(action->getStep());
+}
+
+// Implementation of plumed.getComm
+static PyObject* getComm(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  return PyMPIComm_New(action->comm.Get_comm());
+}
+
+// Implementation of plumed.getMultiSimComm
+static PyObject* getMultiSimComm(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  return PyMPIComm_New(action->multi_sim_comm.Get_comm());
+}
+
+// Implementation of plumed.getKbT
+static PyObject* getKbT(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  return PyFloat_FromDouble(action->plumed.getAtoms().getKbT());
+}
+
+// Implementation of plumed.getNumArgs
+static PyObject* getNumArgs(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  Bias *bias = dynamic_cast<Bias*>(action);
+  plumed_assert(bias);
+  return PyInt_FromLong(bias->getNumberOfArguments());
+}
+
+// Implementation of plumed.getNumExArgs
+static PyObject* getNumExArgs(PyObject* self, PyObject* args)
+{
+  plumed_assert(action);
+  PyBias *bias = dynamic_cast<PyBias*>(action);
+  plumed_assert(bias);
+  return PyInt_FromLong(bias->getNumberOfExtraArguments());
+}
+
+// Python functions
+static PyMethodDef functions[] = {
+    {"getStep", getStep, METH_NOARGS, NULL},
+    {"getComm", getComm, METH_NOARGS, NULL},
+    {"getMultiSimComm", getMultiSimComm, METH_NOARGS, NULL},
+    {"getKbT", getKbT, METH_NOARGS, NULL},
+    {"getNumArgs", getNumArgs, METH_NOARGS, NULL},
+    {"getNumExArgs", getNumExArgs, METH_NOARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+PyBias::PyBias(const ActionOptions& ao):
+PLUMED_BIAS_INIT(ao),
+args(0)
+{
+  // Parse EXTRA keyword for extra arguments
+  if (keywords.exists("EXTRA"))
+  {
+    parseArgumentList("EXTRA", args);
+    log.printf("  with extra arguments");
+    for (unsigned i=0; i<args.size(); i++)
+    {
+      addDependency(args[i]->getPntrToAction());
+      log.printf(" %s", args[i]->getName().c_str());
+    }
+    log.printf("\n");
+  }
+
+  //Initialize Python interpreter
+  if (!Py_IsInitialized())
+    Py_Initialize();
+
+  // Preload Python run-time library
+  // Cannot use DLLLoader, it loads with RTLD_LOCAL flag
+  void* handle = dlopen(LDLIBRARY, RTLD_NOW|RTLD_GLOBAL);
+  if (!handle)
+    plumed_merror(dlerror()); // plumed_massert does not work here!
+  link_map *map;
+  if (dlinfo(handle, RTLD_DI_LINKMAP, &map))
+    plumed_merror(dlerror()); // plumed_massert does not work here!
+  log.printf("  using Python %s %s\n", Py_GetVersion(), map->l_name);
+
+  // Initialize a built-in module
+  PyObject* module = Py_InitModule("plumed", functions); // Borrowed reference
+  plumed_assert(module);
+
+  // Import Numpy
+  import_array();
+  module = PyImport_ImportModule("numpy"); // New reference
+  if (!module)
+  {
+    PyErr_Print();
+    plumed_merror("numpy import failed. See for an error message above");
+  }
+  PyObject* version = PyObject_GetAttrString(module, "__version__"); // New reference
+  plumed_assert(version);
+  PyObject* str = PyObject_Str(module); // New reference
+  plumed_assert(str);
+  Py_DECREF(module);
+  log.printf("  using numpy %s %s\n", PyString_AsString(version),
+             PyString_AsString(str));
+  Py_DECREF(version);
+  Py_DECREF(str);
+
+  // Import mpi4py
+  import_mpi4py();
+  module = PyImport_ImportModule("mpi4py"); // New reference
+  if (!module)
+  {
+    PyErr_Print();
+    plumed_merror("mpi4py import failed. See for an error message above");
+  }
+  version = PyObject_GetAttrString(module, "__version__"); // New reference
+  plumed_assert(version);
+  str = PyObject_Str(module); // New reference
+  plumed_assert(str);
+  Py_DECREF(module);
+  log.printf("  using mpi4py %s %s\n", PyString_AsString(version),
+             PyString_AsString(str));
+  Py_DECREF(version);
+  Py_DECREF(str);
+
+  // Parse FILE and NAME keywords
+  string filename, funcname;
+  parse("FILE", filename);
+  log.printf("  using Python script %s\n", filename.c_str());
+
+  // Check if all keywords have been read
+  checkRead();
+
+  // Set the context to the Python module
+  action = this;
+
+  // Run the Python script
+  FILE* fp = fopen(filename.c_str(), "r");
+  plumed_massert(!PyRun_SimpleFile(fp, filename.c_str()),
+                 "Python script failed. See for an error message above");
+  fclose(fp);
+
+  // Extract the Python function
+  PyObject* main = PyImport_AddModule("__main__");
+  plumed_assert(main);
+  function = PyObject_GetAttrString(main, "bias"); // New reference
+  plumed_massert(function, "Cannot find the Python function in the file");
+  plumed_massert(PyCallable_Check(function),
+                 "The Python function is not callable");
+
+  // Allocate the Numpy arrays
+  npy_intp dims[1];
+  dims[0] = getNumberOfArguments();
+  input = PyArray_SimpleNew(1, &dims[0], NPY_DOUBLE); // New reference
+  plumed_assert(input);
+  force = PyArray_SimpleNew(1, &dims[0], NPY_DOUBLE); // New reference
+  plumed_assert(force);
+  dims[0] = args.size();
+  extra = PyArray_SimpleNew(1, &dims[0], NPY_DOUBLE); // New reference
+  plumed_assert(extra);
+
+  // Setup components
+  addComponent("bias");
+  componentIsNotPeriodic("bias");
+}
+
+PyBias::~PyBias()
+{
+  // Deallocate the Python objects
+  Py_DECREF(function);
+  Py_DECREF(input);
+  Py_DECREF(force);
+  Py_DECREF(extra);
+
+  // Finalize the Python interpreter
+  Py_Finalize();
+}
+
+void PyBias::calculate()
+{
+  // Fill the Numpy arrays with the input values and NaNs
+  for (npy_intp i=0; i<getNumberOfArguments(); i++)
+  {
+    *(npy_double*)PyArray_GETPTR1((PyArrayObject*)input, i) = getArgument(i);
+    *(npy_double*)PyArray_GETPTR1((PyArrayObject*)force, i) = NPY_NAN;
+  }
+
+  // Fill the Numpy array with extra values
+  for (npy_intp i=0; i<(npy_intp)args.size(); i++)
+    *(npy_double*)PyArray_GETPTR1((PyArrayObject*)extra, i) = args[i]->get();
+
+  // Set the action pointer
+  action = this;
+
+  // Call the Python function
+  PyObject* bias = PyObject_CallFunctionObjArgs(function, input, force, extra,
+                                                NULL); // New reference
+  if (!bias)
+  {
+    PyErr_Print();
+    plumed_merror("The Python function failed. See for an error message above");
+  }
+
+  // Process the Python function result
+  plumed_massert(PyFloat_Check(bias), "The Python function has to return a Python float");
+  getPntrToComponent(0)->set(PyFloat_AS_DOUBLE(bias));
+  Py_DECREF(bias);
+
+  // Process the force array
+  for (npy_intp i=0; i<getNumberOfArguments(); i++)
+  {
+    double f = *(npy_double*)PyArray_GETPTR1((PyArrayObject*)force, i);
+    plumed_massert(!npy_isnan(f), "The force array contains a NaN value");
+    setOutputForce(i, f);
+  }
+}
+
+inline unsigned PyBias::getNumberOfExtraArguments() const
+{
+  return args.size();
+}
+
+} // namespace bias
+} // namespace PLMD
